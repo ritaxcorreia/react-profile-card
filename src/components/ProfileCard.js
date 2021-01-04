16c0f05d35b53692c36a47e125a9bd46e4b79fe1
Rita <rita@ritacorreia.co>
2021-01-04 13:45:44 +0000

    final CSS touches to profile card

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -8,22 +8,22 @@ function ProfileCard(props) {
 			<header>
 				<img src={avatar} alt={props.name} />
 			</header>
-			<h1>
-				{props.name} <span className="age">{props.age}</span>
+			<h1 className="bold-text">
+				{props.name} <span className="normal-text">{props.age}</span>
 			</h1>
-			<h2>{props.city}</h2>
+			<h2 className="normal-text">{props.city}</h2>
 			<div className="social-container">
 				<div className="followers">
-					<h1>{props.followers}</h1>
-					<p>Followers</p>
+					<h1 className="bold-text">{props.followers}</h1>
+					<h2 className="smaller-text">Followers</h2>
 				</div>
 				<div className="likes">
-					<h1>{props.likes}</h1>
-					<p>Likes</p>
+					<h1 className="bold-text">{props.likes}</h1>
+					<h2 className="smaller-text">Likes</h2>
 				</div>
 				<div className="photos">
-					<h1>{props.photos}</h1>
-					<p>Photos</p>
+					<h1 className="bold-text">{props.photos}</h1>
+					<h2 className="smaller-text">Photos</h2>
 				</div>
 			</div>
 		</div>
